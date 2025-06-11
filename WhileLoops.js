7196600b4debfc91d88f2aee3b305dcdb479ad39
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-06-10 19:26:20 -0500

    Create WhileLoops.js

---
+++ WhileLoops.js
@@ -0,0 +1,9 @@
+//WHILE LOOPS
+//A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+
+
+/*while (condition) {
+  logic;
+}*/
+
+//Avoid infinite loops--> will lock computer, always have exit condition
