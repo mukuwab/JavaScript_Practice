c401a3de799f86cb6590397b1f578d828ffca493
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-06-10 19:15:05 -0500

    Update IfStatements.js

--- IfStatements.js
+++ IfStatements.js
@@ -56,3 +56,43 @@ if ("") {
 else if(5<10){
   console.log("5 is less than 10");
 }
+
+
+/*p 83
+Sometimes you will want to run different code when all of the if...else if conditions are false. You can do this by adding an else block.
+
+An else block will only evaluate if the conditions in the if and else if blocks are not met.
+
+Here the else block is added to the else if block.*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 > 10) {
+  console.log("5 is less than 10");
+}
+else{
+  console.log("This is the else block");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
+
+
