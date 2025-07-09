0d5d0bfae6f01bf8156d544661e783c1d4232bff
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-09 18:30:08 -0500

    Update ScoreAverage2.js

--- ScoreAverage2.js
+++ ScoreAverage2.js
@@ -8,7 +8,13 @@ function getAverage(scores){
     let sum = 0;//declare and initalize the sum variable outside of the loop so a new instance of the variable is not created for each iteration of the loop
     
     for(const score of scores){
-        
+        //for..of
+            //for...of is a special loop in JavaScript for going through each item in something iterable — like an array or a string.
+            //ex: for (const item of array) {}
+            //For each element inside scores, take its value and temporarily call it score in this loop
+                //The first loop: score is 43...The second loop: score is 56...The third loop: score is 67
+            // Why const? : The name score cannot be reassigned inside one iteration. But each iteration, the loop gives you a new score with the next value in the array.
+            
         sum+=score;
     }
     
@@ -18,7 +24,7 @@ function getAverage(scores){
 }//end getAverage
 
 //print the result the result of the function
-console.log(getAverage(scores));
+console.log("The average score amoung the score array is: "+getAverage(scores));
     //print by calling the function
         //function ()
     //then add parameter
