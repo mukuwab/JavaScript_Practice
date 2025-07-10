6059763748ef2400bce6fac135209f0707abc9e9
Mukuwa Baffoe <mukuwab@yahoo.com>
2025-07-09 19:11:03 -0500

    Update ScoreAverage3.js

--- ScoreAverage3.js
+++ ScoreAverage3.js
@@ -37,3 +37,45 @@ function getGrade(score) {
 console.log(getGrade(96));
 console.log(getGrade(82));
 console.log(getGrade(56));
+
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  let passing;
+  if(score>=60){
+     passing = true;
+  }
+  else {
+    passing =false;
+  }
+  return passing;
+}
+
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
